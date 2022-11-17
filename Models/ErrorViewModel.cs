b3fa4c137fa0a3ca0438d57d3c3e0d94595180fe
AmitabhaPaul <amitabhapaul007@gmail.com>
2022-11-17 11:32:01 +0530

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DockerCompany.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
